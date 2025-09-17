438da3f4582a4630b0c98752309d206eeee972fe
Rory Dent <rory@rock-n-node.com>
2025-09-17 19:42:11 +0200

    testing deploy with vercel

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -1,8 +1,7 @@
 //@ts-check
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { composePlugins, withNx } = require('@nx/next');
-
+const { composePlugins, withNx } = require("@nx/next");
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -10,6 +9,7 @@ const { composePlugins, withNx } = require('@nx/next');
 const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
+  distDir: "dist",
   nx: {},
 };
 
@@ -19,4 +19,3 @@ const plugins = [
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
-
